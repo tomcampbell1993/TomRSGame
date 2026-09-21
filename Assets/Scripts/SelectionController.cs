8af7f2e54baab83269101f52f4783ab543b304ed
Thomas Campbell <tomcampbell1993@hotmail.co.uk>
2026-09-21 16:12:03 +0100

    first multi selection

--- Assets/Scripts/SelectionController.cs
+++ Assets/Scripts/SelectionController.cs
@@ -11,12 +11,16 @@ public class SelectionController : MonoBehaviour
     public BuildingController buildingController;
     public ResourceController resourceController;
 
-    public Unit selectedUnit;
+    public List<Unit> selectedUnits = new List<Unit>();
+
 
     private Tile clickedTile;
     private Unit clickedUnit;
+    private Unit pathingUnit;
     private Building clickedBuilding;
     private Resource clickedResource;
+
+    private Vector2 dragStartPosition; // This is here so that it doesnt change per frame.
     void Start()
     {
 
@@ -24,27 +28,61 @@ public class SelectionController : MonoBehaviour
 
     void Update()
     {
-        HandleLeftClick();
+        HandleLeftMouse();
         HandleRightClick();
+    }
+
+    void HandleLeftMouse()
+    {
+
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            DeSelect();
+            dragStartPosition = Mouse.current.position.ReadValue();
+        }
+
+        if (Mouse.current.leftButton.wasReleasedThisFrame)
+        {
+            Vector2 dragEndPosition = Mouse.current.position.ReadValue();
+            float dragDistance = Vector2.Distance(dragStartPosition, dragEndPosition);
+            if (dragDistance < 10f)
+            {
+                HandleLeftClick();
+            }
+            else
+            {
+                HandleLeftDrag(dragStartPosition, dragEndPosition);
+            }
+
+        }
     }
 
     void HandleLeftClick()
     {
-        if (Mouse.current.leftButton.wasPressedThisFrame)
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        if (Physics.Raycast(ray, out RaycastHit hit))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
-            if (Physics.Raycast(ray, out RaycastHit hit))
+            GetClickedObject(hit);
+            if (clickedUnit != null)
+            {               
+                selectedUnits.Add(clickedUnit);
+            }
+        }
+    }
+
+    void HandleLeftDrag(Vector2 dragStart, Vector2 dragEnd)
+    {
+        float minX = Mathf.Min(dragStart.x, dragEnd.x);
+        float maxX = Mathf.Max(dragStart.x, dragEnd.x);
+        float minY = Mathf.Min(dragStart.y, dragEnd.y);
+        float maxY = Mathf.Max(dragStart.y, dragEnd.y);
+
+        foreach( GameObject unit in unitController.units)
+        {
+            Vector3 unitScreenPosition = Camera.main.WorldToScreenPoint(unit.transform.position);
+            if(unitScreenPosition.x < maxX && unitScreenPosition.x > minX &&  unitScreenPosition.y < maxY && unitScreenPosition.y > minY)
             {
-                GetClickedObject(hit);
-                if (clickedUnit != null)
-                {
-                    if (selectedUnit == clickedUnit)
-                    {
-                        return;
-                    }
-                    DeSelect();
-                    selectedUnit = clickedUnit;
-                }
+                selectedUnits.Add(unit.GetComponent<Unit>());
             }
         }
     }
@@ -57,10 +95,13 @@ public class SelectionController : MonoBehaviour
             if (Physics.Raycast(ray, out RaycastHit hit))
             {
                 GetClickedObject(hit);
-                if (selectedUnit == null)
+                if (selectedUnits.Count == 0)
                 {
                     return;
                 }
+
+                pathingUnit = selectedUnits[0];
+
                 if (clickedTile != null)
                 {
                     HandleMovement();
@@ -90,7 +131,7 @@ public class SelectionController : MonoBehaviour
 
     void DeSelect()
     {
-        selectedUnit = null;
+        selectedUnits.Clear();
     }
 
     void HandleMovement()
@@ -99,29 +140,29 @@ public class SelectionController : MonoBehaviour
 
         if (clickedBuilding != null)
         {
-            targetTile = clickedBuilding.GetClosestSurroundingTile(selectedUnit.transform.position);
+            targetTile = clickedBuilding.GetClosestSurroundingTile(pathingUnit.transform.position);
             if (targetTile == null)
             {
                 return;
             }
-            selectedUnit.targetResource = null;
-            selectedUnit.targetBuilding = clickedBuilding;
+            pathingUnit.targetResource = null;
+            pathingUnit.targetBuilding = clickedBuilding;
         }
 
         else if (clickedResource != null)
         {
-            targetTile = clickedResource.GetClosestTile(selectedUnit.transform.position);
+            targetTile = clickedResource.GetClosestTile(pathingUnit.transform.position);
             if (targetTile == null)
             {
                 return;
             }
-            selectedUnit.targetResource = clickedResource;
-            selectedUnit.targetBuilding = null;
+            pathingUnit.targetResource = clickedResource;
+            pathingUnit.targetBuilding = null;
         }
         else
         {
-            selectedUnit.targetResource = null;
-            selectedUnit.targetBuilding = null;
+            pathingUnit.targetResource = null;
+            pathingUnit.targetBuilding = null;
             targetTile = clickedTile;
         }
 
@@ -129,12 +170,12 @@ public class SelectionController : MonoBehaviour
         {
             return;
         }
-        Tile startTile = tileController.GetTileFromWorldPosition(selectedUnit.transform.position);
+        Tile startTile = tileController.GetTileFromWorldPosition(pathingUnit.transform.position);
         List<Tile> path = unitController.pathfinder.FindPath(startTile, targetTile);
         if (path != null)
         {
             path = unitController.pathfinder.SmoothPath(path);
-            selectedUnit.FollowPath(path);
+            pathingUnit.FollowPath(path);
         }
     }
 }
